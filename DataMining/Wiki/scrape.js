a018afc76cde1647c27e4d41143c5b0122f96941
zsmerritt <zsmerritt@gmail.com>
2018-01-22 21:30:34 -0800

    added notes to Wikipedia page view scrpaer

---
+++ DataMining/Wiki/scrape.js
@@ -0,0 +1,3 @@
+//http://tools.wmflabs.org/pageviews/?project=en.wikipedia.org&platform=all-access&agent=user&start=2018-01-18&end=2018-01-18&pages=Bitcoin|Ethereum
+//This is a link to the wikipedia page views for both Bitcoin and Ethereum. The data tag is "<td class="table-view--average">70,655</td>" and we would need to seperate out the Bitcoin and Ethereum values. 
+// If we have time we should expand this for Litecoin
