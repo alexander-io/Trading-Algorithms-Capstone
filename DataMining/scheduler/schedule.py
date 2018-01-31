08c4f016923bf6777df52237e07fce51b6801ec2
alexander-io <abharris@pugetsound.edu>
2018-01-30 19:43:26 -0800

    add scheduler code

---
+++ DataMining/scheduler/schedule.py
@@ -0,0 +1,9 @@
+import os, subprocess
+
+
+# we are here Trading-Algorithm/DataMining/scheduler/
+# scripts to call here :
+#   Trading-Algorithm/DataMining/Wiki/scrape.js
+#   TODO ... add more relative script paths to call
+
+os.system("~/node-v8.7.0-linux-x64/bin/node ../Wiki/scrape.js")
