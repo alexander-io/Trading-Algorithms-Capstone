22c86495e5e443e2084b568b9e08028f11a8a63d
alexander-io <abharris@pugetsound.edu>
2018-02-21 14:57:41 -0800

    remove progress bar from original scheduler.py

--- DataMining/scheduler/schedule.py
+++ DataMining/scheduler/schedule.py
@@ -1,7 +1,6 @@
 import os, subprocess, time, inspect, pymongo, progressbar
 from threading import Thread
 
-
 # we are here Trading-Algorithm/DataMining/scheduler/
 # scripts to call here :
 
@@ -18,20 +17,12 @@ path=os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 #chop off last 9 charecters (scheduler) from the end
 path=path[0:len(path)-9]
 
-bar = progressbar.ProgressBar(redirect_stdout=True)
-x = 0
 #infinite loop for gathering data
 while True:
-	# update progress indicator
-	bar(range(x))
-	bar.update(x)
-	x+=1
 	#get current time (seconds from 1970, posix/unix/epoch format) and divide by sixty to get minutes
 	currentTime = int(time.time())//60
 	print('current time', currentTime)
 	print('current time mod 86400 ' + str(currentTime%86400))
-
-	print('current time mod 9 ' + str(currentTime%3))
 
 	#once a day stuff
 	if currentTime%86400==0:
