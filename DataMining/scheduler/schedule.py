ab84fb25504ae6e03f042b69ff6319af13c8fcd1
alexander-io <abharris@pugetsound.edu>
2018-02-21 14:25:57 -0800

    inital move to implement multiprocessing in scheduler to prevent blocking, define time tracking related functions: 15minhaselapsed(), 24hourshaselapsed()

--- DataMining/scheduler/schedule.py
+++ DataMining/scheduler/schedule.py
@@ -1,4 +1,4 @@
-import os, subprocess, time, inspect, pymongo
+import os, subprocess, time, inspect, pymongo, progressbar
 from threading import Thread
 
 
@@ -18,20 +18,31 @@ path=os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 #chop off last 9 charecters (scheduler) from the end
 path=path[0:len(path)-9]
 
+bar = progressbar.ProgressBar(redirect_stdout=True)
+x = 0
 #infinite loop for gathering data
 while True:
+	# update progress indicator
+	bar(range(x))
+	bar.update(x)
+	x+=1
 	#get current time (seconds from 1970, posix/unix/epoch format) and divide by sixty to get minutes
 	currentTime = int(time.time())//60
+	print('current time', currentTime)
+	print('current time mod 86400 ' + str(currentTime%86400))
+
+	print('current time mod 9 ' + str(currentTime%3))
+
 	#once a day stuff
 	if currentTime%86400==0:
 		print('executing daily data collection')
 		os.system("node "+path+wiki)
 		os.system("python3 "+path+blockchain)
 	#every 15 min stuff
-	if currentTime%900==0:
+	if currentTime%3==0:
 		print('executing 15min data collection')
 		os.system("node "+path+cmarketcap)
 		os.system("python3 "+path+blockchain)
 		os.system("python3 "+path+twitter)
 
-	time.sleep(60)
+	time.sleep(3)
