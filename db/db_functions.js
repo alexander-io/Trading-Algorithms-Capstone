f6a8f52d5847f4bc42cf2fd12060cd58f72f3553
alexander-io <abharris@pugetsound.edu>
2018-03-13 18:14:29 -0700

    ++add function get_wiki_views_where_pagetitle to resolve object-data from database

--- db/db_functions.js
+++ db/db_functions.js
@@ -9,21 +9,42 @@
   const dbName = 'crypto_trading'
 
   // define insert function
-  module.exports = insert = function(entry, collection_title/*, db, callback*/) {
-    mongo.connect(url, function(err, client) {
-        console.log('successfully connected to server')
-        // define db & collection
-        const db = client.db(dbName)
-        let collection = db.collection(collection_title)
-        // insert entry into db collection
-        collection.insert(entry, function(err, result) {
-          if (err) {
-            console.log('error', err)
-          } else {
-            console.log('inserted ' + entry)
-          }
+  module.exports = {
+    insert : function(entry, collection_title/*, db, callback*/) {
+      mongo.connect(url, function(err, client) {
+          console.log('successfully connected to server')
+          // define db & collection
+          const db = client.db(dbName)
+          let collection = db.collection(collection_title)
+          // insert entry into db collection
+          collection.insert(entry, function(err, result) {
+            if (err) {
+              console.log('error', err)
+            } else {
+              console.log('inserted ' + entry)
+            }
+          })
+          client.close()
         })
-        client.close()
-    })
+      },
+    get_wiki_views_where_pagetitle :  function(pagetitle) {
+      let query = {"pagetitle" : pagetitle}
+      return new Promise(function(resolve, reject) {
+        mongo.connect(url, function(err, client) {
+          if (err) {console.log(err);reject(err)}
+          let collection = client.db(dbName).collection('wiki_views')
+          let x = collection.find(query).toArray()
+          client.close()
+          resolve(x)
+        })
+      })
+    }
   }
+
+  let coins = ['Bitcoin', 'Litecoin', 'Bitcoin_Cash', 'Ripple_(payment_protocol)', 'Dogecoin', 'Ethereum']
+  coins.forEach(function(x) {
+    module.exports.get_wiki_views_where_pagetitle(x).then(function(resolve, reject) {
+      console.log(resolve)
+    })
+  })
 }())
