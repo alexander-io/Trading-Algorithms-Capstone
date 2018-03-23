7288a2354062f2b66f47f9edae6df575948a408d
zsmerritt <zsmerritt@gmail.com>
2018-03-23 10:47:36 -0700

    fixed a few naming errors:

--- PredictionAlgorithms/testing.py
+++ PredictionAlgorithms/testing.py
@@ -11,8 +11,8 @@ def main():
 	with open('pageviewsBTC.csv') as pageViews:
 		for line in pageViews:
 			views.append(float(line))
-	print('robust GLM',AccTest.calculateErrorRobust(views,price,GLM.simple_GLM_WIKI_to_BTC_Value))
-	print('simple GLM',AccTest.calculateErrorSimple(views,price,GLM.simple_GLM_WIKI_to_BTC_Value))
+	print('robust GLM',AccTest.calculateErrorRobust(views,price,GLM.BTC_WIKI_to_BTC_Value))
+	print('simple GLM',AccTest.calculateErrorSimple(views,price,GLM.BTC_WIKI_to_BTC_Value))
 	print('robust NORMAL',AccTest.calculateErrorRobust(views,price,BN.normalized_model_WIKI_to_BTC_value))
 	print('simple NORMAL',AccTest.calculateErrorSimple(views,price,BN.normalized_model_WIKI_to_BTC_value))
 	print('\n\n\n\n\n')
@@ -20,7 +20,7 @@ def main():
 	AccTest.findBestNormalRange(views,price,BN.normalized_model_WIKI_to_BTC_value)
 	print('\n\n\n\n\n')
 	print('glm model')
-	AccTest.findBestNormalRange(views,price,GLM.simple_GLM_WIKI_to_BTC_Value)
+	AccTest.findBestNormalRange(views,price,GLM.BTC_WIKI_to_BTC_Value)
 
 if __name__ == "__main__":
 	# calling main function
