4a62a0c038aabb7f4fed7e3f479a292d20877794
alexander-io <abharris@pugetsound.edu>
2018-03-26 15:35:26 -0700

    update web-app server for basic functionality

--- web_app/serve.js
+++ web_app/serve.js
@@ -1,15 +1,16 @@
-let app = require('express')
+let app = require('express')()
 let server = require('http').createServer(app)
 let io = require('socket.io')(server)
+const port = 8080
+
 io.on('connection', function() {
 
 })
 
-server.listen(8080)
+server.listen(port, function() {
+  console.log('listening on port',)
+})
 
-app.get('/', function(socket)) {
-  socket.emit('data', {data: 'hello world'})
-  socket.on('other data', function(data) {
-    console.log(data)
-  })
-}
+app.get('/', function(req,  res) {
+  res.sendFile(__dirname + '/views/index.html')
+})
