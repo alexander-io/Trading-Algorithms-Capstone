24bf4899dc6bd5c8036e435fd5b517e344f06c00
alexander-io <abharris@pugetsound.edu>
2018-03-26 15:53:55 -0700

    npm install  dependencies : jquery, semantic-ui, express, socket.io. define routes in web_app/serve.js to respond to requests with dependency files, request dependencies on client from server

--- web_app/serve.js
+++ web_app/serve.js
@@ -14,3 +14,19 @@ server.listen(port, function() {
 app.get('/', function(req,  res) {
   res.sendFile(__dirname + '/views/index.html')
 })
+
+app.get('/d3.min.js', function(req, res) {
+  res.sendFile(__dirname + '/node_modules/d3/dist/d3.min.js')
+})
+
+app.get('/semantic.min.js', function(req, res) {
+  res.sendFile(__dirname + '/node_modules/semantic-ui/dist/semantic.min.js')
+})
+
+app.get('/semantic.min.css', function(req, res) {
+  res.sendFile(__dirname + '/node_modules/semantic-ui/dist/semantic.min.css')
+})
+
+app.get('/jquery.min.js', function(req, res) {
+  res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.min.js')
+})
